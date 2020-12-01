8b3a85cbf1ce15d215e893b77584fda513730a78
Alexander <zhiganovaa01@mail.ru>
2020-12-02 02:08:05 +0300

    user init

---
+++ user.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import configparser
+import json
+from telethon.sync import TelegramClient
+from telethon import connection
+
+# для корректного переноса времени сообщений в json
+from datetime import date, datetime
+
+# классы для работы с каналами
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# класс для работы с сообщениями
+from telethon.tl.functions.messages import GetHistoryRequest
+
+from config import api_id_alex, api_hash_alex, api_hash_main, api_id_main, id_main, id_alex, username_main, username_alex
+
+
+client = TelegramClient(username_main, api_id_main, api_hash_main)
+#client = TelegramClient(username_alex, api_id_alex, api_hash_alex)
+client.start()
