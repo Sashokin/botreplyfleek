491a9bc749d5eb6c87200ff7475345c9bcff0c09
Alexander <zhiganovaa01@mail.ru>
2020-12-01 23:49:02 +0300

    setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # botreplyfleek
 Пересылка сообщений из частных канал в телеге
+
+# для начала работы - 'pip install -r requirements.txt' в терминал
---
+++ bot.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import time
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import bot_token
+
+# todo:
+
+
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def process_bot_check(message: types.Message):
+    await message.reply("bruh im working")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
