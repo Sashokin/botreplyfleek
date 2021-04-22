b14981be2d17fd0316fa8391f77b4471f8b2e231
Alexander <zhiganovaa01@mail.ru>
2021-04-22 13:12:18 +0300

    hehe

---
+++ config.py
@@ -0,0 +1,28 @@
+api_id_main = '2708393'
+id_main = 680180012
+api_hash_main = '07bfdcf8deff44b63cb1e5a1ba82ebc2'
+username_main = 'gfrtsybgggbbhbbbotricauvas'
+
+api_id_alex = '2614513'
+id_alex = 1147419570
+api_hash_alex = '8bb166d608950b1fdac15771ae8e9160'
+username_alex = 'tinkoffinv'
+
+api_id_aslan = ''
+id_aslan = 0
+api_hash_aslan = ''
+username_aslan = ''
+
+api_id_anton = '2086340'
+id_anton = 0
+api_hash_anton = '4a328f9003f11b8012cc5430e012bfc2'
+username_anton = 'antonio'
+
+
+id_testchannel = -1001340694970
+id_leakchannel = -1001359483835
+id_fleek = -1001169026621
+id_tesla = -1001283065049
+
+
+DATABASE_URL = ''
